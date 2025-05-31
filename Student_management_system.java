3336675b5bdfcecc7864ac38f981aa033f54ba00
BasudhaMitra27 <mitrabasudha87@gmail.com>
2025-05-31 10:35:43 +0530

    Student_management_system.java

---
+++ Student_management_system.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    @override
+    public String tostring() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade:
+    }
+}
+
+class StudentManagementSystem {
+    private List<Student> students;
+
+    public StudentManagementSystem() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public boolean removeStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                students.remove(student);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public student searchstudent(int rollNumber) {
+      for (Student student : students) {
+        if (student.getRollNumber() == rollNumber) {
+          return student;
+        }
+      }
+      return null;
+    }
+
